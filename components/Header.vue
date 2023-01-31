ef6cbaa86510ba70f3d5b0a44ec4a961a27fd9fe
JasonTongg <aboytong123@gmail.com>
2023-01-31 21:01:19 +0700

    done

---
+++ components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <header class="header">
+      <h1 class="header-title">Food Recipes</h1>
+      <nav class="header-nav">
+        <nuxtLink tag="button" to="/" class="header-nav__link"> Home </nuxtLink>
+        <nuxtLink tag="button" to="/add" class="header-nav__link">
+          Add Recipes
+        </nuxtLink>
+        <nuxt-link
+          tag="a"
+          to="/user/login"
+          class="header-nav__link"
+          v-if="!$store.getters.isAuthenticated"
+        >
+          Login
+        </nuxt-link>
+        <a
+          class="header-nav__link"
+          v-if="$store.getters.isAuthenticated"
+          @click="logout"
+        >
+          Logout
+        </a>
+        <a class="header-nav__link" v-if="$store.getters.isAuthenticated">
+          {{ $store.state.userData.username }}
+        </a>
+      </nav>
+    </header>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.commit("setToken", null);
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+<style>
+.body {
+  margin: 0px;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 40px;
+  padding-right: 40px;
+  background-color: #eef2e6;
+}
+
+main {
+  margin: 22px;
+}
+
+.recipes {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.header-nav__link {
+  margin-left: 20px;
+  color: black;
+  text-decoration: none;
+  font-size: 20px;
+}
+</style>
---
+++ store/index.js
@@ -0,0 +1,149 @@
+import axios from "axios";
+
+export const state = () => ({
+  /* 
+    recipes: [
+        {
+            id: 1,
+            recipeImage: "https://i.ibb.co/SBsMYNC/Rendang.jpg",
+            recipeTitle: "Rendang",
+            likes: 100,
+            body: "Rendang Recipe",
+        },
+        {
+            id: 2,
+            recipeImage: "https://i.ibb.co/MRNhgzW/Tomyam.jpg",
+            recipeTitle: "Tomyam",
+            likes: 40,
+            body: "Tomyam Recipe",
+        },
+        {
+            id: 3,
+            recipeImage: "https://i.ibb.co/CW4tVvp/Spaghetti-aglioo-o-lio.jpg",
+            recipeTitle: "Spagethi Aglio Olio",
+            likes: 200,
+            body: "Spagethi Aglio Olio Recipe",
+        },
+        {
+            id: 4,
+            recipeImage: "https://i.ibb.co/z7zRVxV/Spaghetti-Carbonara.jpg",
+            recipeTitle: "Spagethi Carbonara",
+            likes: 200,
+            body: "Spagethi Carbonara Recipe",
+        },
+        {
+            id: 5,
+            recipeImage: "https://i.ibb.co/Cn1XPNB/Kimchi.jpg",
+            recipeTitle: "Kimchi",
+            likes: 10,
+            body: "Kimchi Recipe",
+        },
+    ],
+*/
+  recipes: [],
+  token: null,
+  userData: null,
+});
+
+export const getters = {
+  recipeData(state) {
+    return state.recipes;
+  },
+
+  lastIdRecipe(state) {
+    let recipeLength = state.recipes.length;
+    return state.recipes[recipeLength - 1].id;
+  },
+
+  detailRecipe: (state) => (id) => {
+    return state.recipes.find((recipe) => recipe.id === id);
+  },
+
+  isAuthenticated(state) {
+    return state.token != null;
+  },
+};
+
+export const mutations = {
+  addNewRecipe(state, payload) {
+    return state.recipes.push(payload);
+  },
+
+  setRecipe(state, payload) {
+    state.recipes = payload;
+  },
+
+  setToken(state, payload) {
+    state.token = payload;
+  },
+
+  setUserData(state, payload) {
+    state.userData = payload;
+  },
+};
+
+export const actions = {
+  // nuxtServerInit({commit}) {
+  //     return axios.get("https://vue-js-project-9f7db-default-rtdb.firebaseio.com/datarecipe")
+  //                 .then(response => {
+  //                         const recipeArray = [];
+  //                         for (const key in response.data) {
+  //                             recipeArray.push({...response.data[key], id: key})
+  //                             }
+  //                     commit('setRecipe', recipeArray)
+  //                     })
+  //                 .catch(e => context.error(e))
+  // }
+  nuxtServerInit({ commit }) {
+    return axios
+      .get(
+        "https://vue-js-project-9f7db-default-rtdb.firebaseio.com/datarecipe.json"
+      )
+      .then((response) => {
+        const recipeArray = [];
+        for (const key in response.data) {
+          recipeArray.push({ ...response.data[key], id: key });
+        }
+        commit("setRecipe", recipeArray);
+      })
+      .catch((e) => context.error(e));
+  },
+
+  authenticateUser({ commit }, authData) {
+    let webAPIKey = "AIzaSyDpfWzk5wlzHGn63kUnntrviYdiVSSYS8s";
+    let authUrl = authData.isLogin
+      ? "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key="
+      : "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+
+    return axios
+      .post(authUrl + webAPIKey, {
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true,
+        displayName: authData.displayName,
+      })
+      .then((response) => {
+        commit("setToken", response.data.idToken);
+        commit("setUserData", {
+          username: response.data.displayName,
+          userId: response.data.localId,
+          email: response.data.email,
+        });
+        console.log(response.data);
+        alert(authData);
+      })
+      .catch((error) => alert(error.response.data.message));
+  },
+
+  addRecipe({ commit, state }, recipe) {
+    return axios
+      .post(
+        "https://vue-js-project-9f7db-default-rtdb.firebaseio.com/datarecipe.json?auth=" +
+          state.token,
+        recipe
+      )
+      .then((response) => {
+        commit("addNewRecipe", recipe);
+      });
+  },
+};
